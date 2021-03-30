c6b15177d3251d5ab25eda8d253667c5c1e2ae15
S. CM <33370667+phisanti@users.noreply.github.com>
2021-03-30 21:01:45 +0100

    Change launch command to /init

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ fi
 # Note: print mocked jupyter token so that we can run this container as if it is a notebook within Gradient V1
 echo "http://localhost:8888/?token=${JUPYTER_TOKEN}"
 echo "http://localhost:8888/\?token\=${JUPYTER_TOKEN}"
+
 #--www-port=8888 --auth-none=1 --server-daemonize=0 --server-data-dir="$RSTUDIO_SERVER_DATA_DIR" --server-pid-file="$RSTUDIO_SERVER_DATA_DIR/rstudio-server.pid"
 # jupyter lab --ip=0.0.0.0 --NotebookApp.token='local-development' --allow-root --no-browser &> /dev/null &
-# PASSWORD=${JUPYTER_TOKEN} /usr/lib/rstudio-server/bin/rserver --www-port=8888 --auth-none=1 --server-daemonize=0 --www-address=0.0.0.0
+
+/init
